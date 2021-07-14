6768bfb785a70944ebb662c5f852ea30a8066e45
Vyacheslav Onufrienko <onufrienkovi@gmail.com>
2021-07-14 18:05:06 +0300

    review

--- account/views.py
+++ account/views.py
@@ -10,18 +10,6 @@ from .models import User
 
 
 def index(request):
-    users_query = User.objects.all()
-    url = "https://randomuser.me/api/?results=100"
-    data = requests.get(url).json()["results"]
-    while len(users_query) < 100:
-        for user_data in data:
-            user = User.objects.get_or_create(
-                username=user_data.get("login", {}).get("username", ""),
-                password=user_data.get("login", {}).get("password", ""),
-                first_name=user_data.get("name", {}).get("first", ""),
-                last_name=user_data.get("name", {}).get("last", ""),
-                gender=user_data.get("gender", ""),
-            )
     data_objects = User.objects.all()
     my_filter = OrderFilter(request.GET, queryset=data_objects)
     data_objects = my_filter.qs
